6fcd4fa6ea38dc16a0061777cef8b5dd863ca472
megmgleason <marymargaretgleason@gmail.com>
2022-09-26 15:30:49 -0400

    Initial Commit

---
+++ HW8/HW8App.swift
@@ -0,0 +1,17 @@
+//
+//  HW8App.swift
+//  HW8
+//
+//  Created by MEG G on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct HW8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
